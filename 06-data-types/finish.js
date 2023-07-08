47c186791434dc0feaab2751b74d72c8233a7021
Michael Serhiienko <fleshrobot7@gmail.com>
2023-07-08 17:05:45 +0300

    first commit

---
+++ 06-data-types/finish.js
@@ -0,0 +1,27 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+let sting = "string";
+let number = 5;
+let boolean = true;
+let nulll = null;
+let und = undefined;
+let object = {
+  meName: "Michael",
+};
+let mass = [1, 2, 3, 4];
+
+console.log(mass);
+console.log(object);
